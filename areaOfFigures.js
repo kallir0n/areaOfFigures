74580788969ada6e40b07ee252b02966cf981021
Сергей Козлов <s.kozlov@bars.group>
2024-08-19 00:21:13 +0300

    добавлена функция whoami и исправлены баги генерации

--- areaOfFigures.js
+++ areaOfFigures.js
@@ -1,79 +1,88 @@
 class Figure{
-    get area() {
-        return this.calcArea();
-      }
-
     calcArea() {
         return ('Невозможно рассчитать площадь')
     }
 }
 
 class triangle extends Figure {
-    constructor(katetFirst, katetSecond, gipotenuza) {
+    constructor(katetFirst, katetSecond, gipotenuza, vertices) {
         super();
         this.gipotenuza = gipotenuza;
         this.katetSecond = katetSecond;
         this.katetFirst = katetFirst;
+        this.vertices = vertices;
     }
 
-    #gipotenuza;
-
-    #katetFirst;
-
-    #katetSecond;
-
     calcArea(katetFirst, katetSecond, gipotenuza) {
-        let p = (katetFirst + katetSecond + gipotenuza)/2;
+        let p = (katetFirsts + katetSecond + gipotenuza)/2;
         return Math.sqrt(p*(p-katetFirst)*(p-katetSecond)*(p-gipotenuza));
+    }
+    whoami(vertices) {
+        return {
+            id: Math.random(1,1000),
+            vertices: vertices,
+            class: 'triangle',
+        }
     }
 }
 
 class rectangle extends Figure{
-    constructor(height, weight) {
+    constructor(height, weight, vertices) {
         super();
         this.weight = weight;
         this.height = height;
+        this.vertices = vertices;
     }
-    #height;
-    #weight;
 
     calcArea(height, weight) {
         return height * weight;
     }
+    whoami(vertices) {
+        return {
+            id: Math.random(1,1000),
+            vertices: vertices,
+            class: 'rectangle',
+        }
+    }
 }
 
 class circle extends Figure {
-    constructor(radius) {
+    constructor(radius, vertices) {
         super();
+        this.vertices = vertices
         this.radius = radius;
     }
-
-    #radius;
 
     calcArea(radius) {
         return Math.PI * Math.pow(radius, 2);
     }
+    whoami(vertices) {
+        return {
+            id: Math.random(1,1000),
+            vertices: vertices,
+            class: 'circle',
+        }
+    }
 }
 
-function generateFigure() {
-    let vertices = [];
+function generateFigure(figureCount) {
+    let figures = [];
     let generated = getRandomInt(0, 4);
     let i = 0;
-    for (;i < 20;) {
+    for (;i < figureCount;) {
         if (generated !== 1 && generated !== 2) {
             switch (generated){
                 case 0:
-                    let circleFigure = new circle(getRandomInt(1, 25));
-                    circleFigure.
-                    vertices.push(circleFigure);
+                    let circleFigure = new circle(getRandomInt(1, 25), generated);
+                    figures.push(circleFigure);
                     break;
                 case 3:
-                    let triangleFigure = new triangle(getRandomInt(1, 25), getRandomInt(1, 25), getRandomInt(1, 25));
-                    vertices.push(triangleFigure);
+                    let triangleFigure = new triangle(getRandomInt(1, 25), getRandomInt(1, 25), getRandomInt(1, 25), generated);
+                    figures.push(triangleFigure);
                     break;
                 case 4:
-                    let rectangleFigure = new rectangle(getRandomInt(1, 25), getRandomInt(1, 25));
-                    vertices.push(rectangleFigure);
+                    let rectangleFigure = new rectangle(getRandomInt(1, 25), getRandomInt(1, 25), generated);
+                    figures.push(rectangleFigure);
                     break;
                 default:
                     break;
@@ -83,16 +92,27 @@ function generateFigure() {
         generated = getRandomInt(0, 4);
     }
 
-    return vertices;
+    return figures;
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
+function whoami(fig) {
+    if (fig) {
+        return fig.whoami(fig.vertices)
+    }
+}
 
-
-console.log(generateFigure());
+let figures = generateFigure(20);
+let whoamiFig;
+for (fig of figures) {
+    whoamiFig = whoami(fig);
+    console.log (whoamiFig)
+}
+console.log(figures);
+console.log(generateFigure(20));
 
